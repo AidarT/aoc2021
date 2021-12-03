32839e328ddb216a1719a6aef4addc549d1807f2
Aidar Tukmeev <aidar.tu.n@gmail.com>
2021-12-03 16:37:00 +0300

    day 3. refactor

--- aoc2021day3.py
+++ aoc2021day3.py
@@ -1,59 +1,37 @@
-import re
-
 with open('C:/Users/User/Documents/input.txt') as f:
-    my_input = list(map(lambda a: list(map(lambda a: int(a), re.findall(r'.', a))), f.read().split('\n')))
+    my_input = f.read().split('\n')
 f.close()
 
 
-def find_rate_p2(input, flag):
-    temp_input = input.copy()
-    rate_str = ""
-    for x in range(0, len(input[0]), 1):
-        zero_count = 0
-        one_count = 0
-        if x > 0:
-            for y in range(len(temp_input) - 1, -1, -1):
-                for i in range(0, x, 1):
-                    if temp_input[y][i] != int(rate_str[i]):
-                        temp_input.pop(y)
-                        break
-        if len(temp_input) == 1:
-            temp_input[0] = [str(num) for num in temp_input[0]]
-            rate_str = "".join(temp_input[0])
-            return rate_str
-        for y in range(0, len(temp_input), 1):
-            if temp_input[y][x] == 1:
-                one_count = one_count + 1
-            else:
-                zero_count = zero_count + 1
-        if one_count >= zero_count:
-            rate_str = rate_str + str(flag)
-        else:
-            rate_str = rate_str + str(~flag + 2)
-    return rate_str
+def invert_int(n):
+    return ~n & (2 ** n.bit_length()) - 1
 
 
-gamma_rate = ""
-epsilon_rate = ""
-for x in range(0, len(my_input[0]), 1):
-    zero_count = 0
-    one_count = 0
-    for y in range(0, len(my_input), 1):
-        if my_input[y][x] == 1:
-            one_count = one_count + 1
-        else:
-            zero_count = zero_count + 1
-    if one_count > zero_count:
-        gamma_rate = gamma_rate + "1"
-        epsilon_rate = epsilon_rate + "0"
+def resolve_needed_bits(numbers, flag, equal_check):
+    count = []
+    for bit_num in range(0, len(numbers[0]), 1):
+        ones_input = list(filter(lambda num: num[bit_num] == '1', numbers))
+        count.append(str(len(ones_input)))
+    if equal_check:
+        return "".join([str(flag) if float(flag_count) >= len(numbers) / 2 else str(~flag + 2) for flag_count in count])
     else:
-        gamma_rate = gamma_rate + "0"
-        epsilon_rate = epsilon_rate + "1"
+        return "".join([str(flag) if float(flag_count) > len(numbers) / 2 else str(~flag + 2) for flag_count in count])
 
-part1 = int(gamma_rate, 2) * int(epsilon_rate, 2)
 
-O2_rate = find_rate_p2(my_input, 1)
-CO2_rate = find_rate_p2(my_input, 0)
+def find_last_num(numbers, flag, equal_check):
+    for bit_num in range(0, len(numbers[0]), 1):
+        check = resolve_needed_bits(numbers, flag, equal_check)
+        numbers = [num for num in numbers if check[bit_num] == num[bit_num]]
+        if len(numbers) == 1:
+            return numbers[0]
+
+
+gamma_rate = int(resolve_needed_bits(my_input, 1, False), 2)
+epsilon_rate = invert_int(gamma_rate)
+part1 = gamma_rate * epsilon_rate
+
+O2_rate = find_last_num(my_input, 1, True)
+CO2_rate = find_last_num(my_input, 0, True)
 
 part2 = int(O2_rate, 2) * int(CO2_rate, 2)
 
