ad5f61bdb1211280cd02b1e073cb048bacb75554
Aidar Tukmeev <aidar.tu.n@gmail.com>
2021-12-17 17:41:39 +0300

    day 16. part2 beginning

--- aoc2021day16.py
+++ aoc2021day16.py
@@ -50,42 +50,15 @@ def parse_packet(message):
             gr_num += message[last_bit + 1: last_bit + 5]
             last_bit += 5
         message = message[last_bit:]
-        gr_num = int(gr_num, 2)
-    else:
-        len_type_id = message[6]
-        if len_type_id == '0':
-            total_length = int(message[7:22], 2)
-            sub = message[22:22 + total_length]
-            ver += parse_packet(sub)
-            message = message[22 + total_length:]
-        else:
-            ver += parse_packet(message[18:])
-            return ver
-    if message == '' or message.count('0') == len(message):
-        return ver
-    return ver + parse_packet(message)
-
-
-part1 = parse_packet(my_input)
-
-
-def parse_packet_p2(message):
-    ver = int(message[0:3], 2)
-    type_id = int(message[3:6], 2)
-    if type_id == 4:
-        gr_type = message[6]
-        gr_num = message[7:11]
-        last_bit = 11
-        while gr_type != '0':
-            gr_type = message[last_bit]
-            gr_num += message[last_bit + 1: last_bit + 5]
-            last_bit += 5
-        message = message[last_bit:]
     else:
         len_type_id = message[6]
         total_length = int(message[7:22], 2) if len_type_id == '0' else int(message[7:18], 2)
         message = message[22:] if len_type_id == '0' else message[18:]
         nums = []
+        type_id_sub = int(message[3:6], 2)
+        if type_id_sub != 4:
+            parsed = parse_packet(message)
+            return ver + parsed[0], parsed[1]
         while total_length > 0:
             ver_sub, last_bit, num = unpack_sub(message)
             nums.append(num)
@@ -99,6 +72,6 @@ def parse_packet_p2(message):
     return ver + parsed[0], parsed[1]
 
 
-part2 = parse_packet_p2(my_input)[1]
+part1, part2 = parse_packet(my_input)
 
 print(str(part1) + " " + str(part2))
